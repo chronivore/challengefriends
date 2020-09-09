b6a2950377b420e1b2f2bca832fe18e2b2e2711b
Nate Gropp <nathangropp@gmail.com>
2020-09-09 09:27:39 -0400

    wednesday morning

--- src/Components/Location.js
+++ src/Components/Location.js
@@ -1,12 +1,12 @@
 import React, {useState, useEffect} from 'react';
 
-
 const Location = () => {
+
+    
     return (
         <div>
-            
+
         </div>
     )
 }
-
 export default Location;
